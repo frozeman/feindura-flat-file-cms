06ff575b6ae7bcfe719c41ea94ff0f6ca7896cb7
Fabian Vogelsteller <feindura@frozeman.de>
2010-04-05 19:42:15 +0200

    reedit of build 146
    Signed-off-by: Fabian Vogelsteller <feindura@frozeman.de>

---
+++ config/admin.config.php
@@ -0,0 +1,35 @@
+<?php
+
+$adminConfig['url'] =             'localhost';
+$adminConfig['basePath'] =        '/_feindura10/';
+$adminConfig['savePath'] =        '/_feindura10/_page/';
+$adminConfig['uploadPath'] =      '/_feindura10/_upload/';
+$adminConfig['websitefilesPath'] =        '/_feindura10/addons/';
+$adminConfig['stylesheetPath'] =  '/_feindura10/library/style/';
+$adminConfig['dateFormat'] =      'eu';
+$adminConfig['speakingUrl'] =      '';
+
+$adminConfig['varName']['page'] =     'page';
+$adminConfig['varName']['category'] = 'category';
+$adminConfig['varName']['modul'] =    'modul';
+
+$adminConfig['user']['editLanguage'] =    'true';
+$adminConfig['user']['editStylesheet'] =  '';
+$adminConfig['user']['info'] =            '';
+
+$adminConfig['setStartPage'] =            'true';
+$adminConfig['page']['createPages'] =     'true';
+$adminConfig['page']['thumbnailUpload'] = '';
+
+$adminConfig['editor']['enterMode'] =   'p';
+$adminConfig['editor']['styleFile'] =   '/mattis/style/layout.css';
+$adminConfig['editor']['styleId'] =     '';
+$adminConfig['editor']['styleClass'] =  'feinduraStyles';
+
+$adminConfig['pageThumbnail']['width'] =      '100';
+$adminConfig['pageThumbnail']['height'] =     '30';
+$adminConfig['pageThumbnail']['ratio'] =      'x';
+$adminConfig['pageThumbnail']['path'] =       'images/thumbnail/';
+
+return $adminConfig;
+?>
--- library/backend.include.php
+++ library/backend.include.php
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License along with this program;
     if not,see <http://www.gnu.org/licenses/>.
 *
-* backend.include.php version 0.22
+* backend.include.php version 0.23
 */
 
 error_reporting(E_ALL & ~E_NOTICE); //E_ALL & ~E_NOTICE
