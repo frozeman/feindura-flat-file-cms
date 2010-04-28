ba86b4c95db78a12b08185894f846b0976bbf49c
Fabian Vogelsteller <fabian@feindura.org>
2010-04-28 20:45:01 +0200

    build 173: startet to change feindura.class.php and add feinduraPages.class.php
    Signed-off-by: Fabian Vogelsteller <fabian@feindura.org>

--- library/frontend.include.php
+++ library/frontend.include.php
@@ -23,5 +23,6 @@ require_once(dirname(__FILE__)."/general.include.php");
 require_once(dirname(__FILE__)."/functions/mysql.functions.php");
 
 require_once(dirname(__FILE__)."/classes/feindura.class.php");
+require_once(dirname(__FILE__)."/classes/feinduraPages.class.php");
 
 ?>
