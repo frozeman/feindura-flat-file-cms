7db985d4beb2ab5b5039872a40b0ceb3954443de
Fabian Vogelsteller <fabian@feindura.org>
2010-05-13 23:04:42 +0200

    build 201: add check if plugins or modules folder is empty
    Signed-off-by: Fabian Vogelsteller <fabian@feindura.org>

--- index.php
+++ index.php
@@ -188,7 +188,7 @@ if($_GET['site'] == 'addons') {
           <td><a href="?site=home" class="home" title="<?php echo $langFile['btn_home']; ?>"><span><?php echo $langFile['btn_home']; ?></span></a></td>
           <td><a href="?site=pages" class="pages" title="<?php echo $langFile['btn_pages']; ?>"><span><?php echo $langFile['btn_pages']; ?></span></a></td>
           <?php
-          // checks if there any modules or plugins, if not dont show the menupoint
+          // CHECKS if the addons/ folder is empty
           if(!folderIsEmpty($adminConfig['basePath'].'addons/')) { ?>
           <td><a href="?site=addons" class="addons" title="<?php echo $langFile['btn_addons']; ?>"><span><?php echo $langFile['btn_addons']; ?></span></a></td>
           <?php } ?>
@@ -224,9 +224,25 @@ if($_GET['site'] == 'addons') {
           <tr>
           <td><a href="?site=adminSetup" class="adminSetup" title="<?php  echo $langFile['btn_adminSetup']; ?>"><span><?php echo $langFile['btn_adminSetup']; ?></span></a></td>
           <td><a href="?site=pageSetup" class="pageSetup" title="<?php  echo $langFile['btn_pageSetup']; ?>"><span><?php echo $langFile['btn_pageSetup']; ?></span></a></td>
+          <?php
+          // CHECKS if one of the plugins/ or modules/ folders is empty
+          if(!folderIsEmpty($adminConfig['basePath'].'plugins/') || !folderIsEmpty($adminConfig['basePath'].'modules/')) { ?>
           </tr><tr>
+          <?php
+          // CHECKS if the plugins/ folder is empty
+          if(!folderIsEmpty($adminConfig['basePath'].'plugins/')) { ?>
           <td><a href="?site=pluginSetup" class="pluginSetup" title="<?php  echo $langFile['btn_pluginSetup']; ?>"><span><?php echo $langFile['btn_pluginSetup']; ?></span></a></td>
+          <?php } else { ?>
+          <td</td>
+          <?php }
+
+          // CHECKS if the plugins/ folder is empty
+          if(!folderIsEmpty($adminConfig['basePath'].'modules/')) { ?>
           <td><a href="?site=modulSetup" class="modulSetup" title="<?php  echo $langFile['btn_modulSetup']; ?>"><span><?php echo $langFile['btn_modulSetup']; ?></span></a></td>
+          <?php } else { ?>
+          <td</td>
+          <?php }
+          } ?>
           </tr><tr>
           <td><a href="?site=statisticSetup" class="statisticSetup" title="<?php  echo $langFile['btn_statisticSetup']; ?>"><span><?php echo $langFile['btn_statisticSetup']; ?></span></a></td>
           <td><a href="?site=userSetup" onclick="openWindowBox('library/sites/userSetup.php','<?php echo $langFile['btn_userSetup']; ?>',true);return false;" class="userSetup" title="<?php echo $langFile['btn_userSetup']; ?>"><span><?php echo $langFile['btn_userSetup']; ?></span></a></td>
