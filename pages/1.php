cc5ad387221af1a66f4e834408cad74f1e1d6c74
SamGoody <siteroller@gmail.com>
2010-12-09 11:14:43 +0200

    For testing, changed home page's content. Why is this saved?

--- pages/1.php
+++ pages/1.php
@@ -5,8 +5,8 @@ $pageContent['category'] =           0;
 $pageContent['public'] =             true;
 $pageContent['sortorder'] =          3;
 
-$pageContent['lastsavedate'] =       '1287431111';
-$pageContent['lastsaveauthor'] =     'frozeman';
+$pageContent['lastsavedate'] =       '1291807388';
+$pageContent['lastsaveauthor'] =     '';
 
 $pageContent['title'] =              'Welcome';
 $pageContent['description'] =        '';
@@ -21,22 +21,22 @@ $pageContent['styleFile'] =          '';
 $pageContent['styleId'] =            '';
 $pageContent['styleClass'] =         '';
 
-$pageContent['log_visitorcount'] =   '3';
-$pageContent['log_visitTime_min'] =  'a:3:{i:0;i:7;i:1;i:7;i:2;i:0;}';
-$pageContent['log_visitTime_max'] =  'a:2:{i:0;i:19;i:1;i:7;}';
+$pageContent['log_visitorcount'] =   '6';
+$pageContent['log_visitTime_min'] =  'a:10:{i:0;i:1062;i:1;i:278;i:2;i:263;i:3;i:212;i:4;i:161;i:5;i:116;i:6;i:88;i:7;i:76;i:8;i:31;i:9;i:15;}';
+$pageContent['log_visitTime_max'] =  'a:4:{i:0;i:73944;i:1;i:348;i:2;i:19;i:3;i:7;}';
 $pageContent['log_firstVisit'] =     '1290977875';
-$pageContent['log_lastVisit'] =      '1291061972';
+$pageContent['log_lastVisit'] =      '1291801914';
 $pageContent['log_searchwords'] =    '';
 
 $pageContent['content'] = 
-'
-Test te<br />
-
-nvbnb<br />
-
-fdgfdgdfgfdgdfg dfg dfgfd g,m idjf&ouml;kdfk g&auml;jfd&ouml;g kdfgfdg<br />
-
-xt
+'<p>
+	&nbsp;</p>
+<p>
+	This is the welcome page.</p>
+<p>
+	lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;</p>
+<p>
+	lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;lorem ipsum dolor sit amet&nbsp;</p>
 ';
 
 return $pageContent;
