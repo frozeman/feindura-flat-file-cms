5c9e0193594856488f0a0f055ad8b306e790c7b2
Fabian Vogelsteller <fabian@frozeman.de>
2011-04-18 12:03:51 +0200

    add api doc examples
    Signed-off-by: Fabian Vogelsteller <fabian@frozeman.de>

---
+++ doc/examples/createMenuBySortFunction.example.php
@@ -0,0 +1,62 @@
+<?php
+/*                               *** CODE *** 
+--------------------------------------------------------------------------------
+This example uses all possible properties.
+It's also works much more simple: just call createMenu('category',1)
+and you have a simple array with links of the pages from this category.
+*/
+
+// a session will be started in the "feindura.include.php",
+// therefor you have to include this file before the header of the HTML page is sent,
+// which means before any HTML Tag.
+require('cms/feindura.include.php');
+
+// creates a new feindura instance
+$myCms = new feindura();
+
+// create the sort function, which we use with the createMenuBySortFunction() method
+function sortByLastEditDate($a,$b) {
+  if ($a['lastSaveDate'] == $b['lastSaveDate'])
+    return 0;
+  return ($a['lastSaveDate'] > $b['lastSaveDate']) ? -1 : 1;
+}
+
+// now we create the menu from the category with ID "1"
+$menu = $myCms->createMenuBySortFunction('sortByLastEditDate','category',1,'table',true,2);
+
+// displays the menu
+foreach($menu as $link) {
+  echo $link;
+}
+
+
+
+/*                              *** RESULT *** 
+--------------------------------------------------------------------------------
+*/
+
+<table>
+<tr><td>
+<a href="?category=1&amp;page=2" title="Example Page 2">
+Example Page 2
+</a>
+</td>
+<td>
+<a href="?category=1&amp;page=1" title="Example Page 1">
+Example Page 1
+</a>
+</td>
+</tr><tr>
+<td>
+<a href="?category=1&amp;page=4" title="Example Page 4">
+Example Page 4
+</a>
+</td>
+<td>
+<a href="?category=1&amp;page=3" title="Example Page 3">
+Example Page 3
+</a>
+</td></tr>
+</table>
+
+?>
---
+++ doc/examples/listPagesBySortFunction.example.php
@@ -0,0 +1,68 @@
+<?php
+/*                               *** CODE *** 
+--------------------------------------------------------------------------------
+This example uses all possible properties.
+It's also works much more simple: just call listPages() without setting properties
+and list the current category given by $_GET variable.
+*/
+
+// a session will be started in the "feindura.include.php",
+// therefor you have to include this file before the header of the HTML page is sent,
+// which means before any HTML Tag.
+require('cms/feindura.include.php');
+
+// creates a new feindura instance
+$myCms = new feindura();
+
+// create the sort function, which we use with the listPagesBySortFunction() method
+function sortByLastEditDate($a,$b) {
+  if ($a['lastSaveDate'] == $b['lastSaveDate'])
+    return 0;
+  return ($a['lastSaveDate'] > $b['lastSaveDate']) ? -1 : 1;
+}
+
+// now we return the pages from the category with ID "1" and "2"
+// the page content will be shorten to "200" characters
+$pages = $myCms->listPagesBySortFunction('sortByLastEditDate','category',array(1,2),200);
+
+// displays the pages (the "\n" creates a line break for a better look)
+foreach($pages as $page) {
+  echo $page['title']."\n\n";
+  echo $page['thumbnail']."\n";
+  echo $page['content']."<br />-----------------------<br />\n";
+}
+
+
+/*                              *** RESULT with page *** 
+--------------------------------------------------------------------------------
+*/
+
+<a href="?category=1&amp;page=2" title="Example Page 2">
+Example Page 2
+</a>
+<h2>Example Headline</h2>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing dolores et ea rebum.
+Stet clita kasd gubergren, no sea takimata sanctus.</p>
+<a href="?category=1&amp;page=2">mehr</a>
+<br />-----------------------<br />
+
+<a href="?category=1&amp;page=3" title="Example Page 3">
+Example Page 3
+</a>
+<h2>Another Example Headline</h2>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing dolores et ea rebum.</p>
+<h2>And one more Example Headline</h2>
+<p>Stet clita kasd gubergren, no sea takimata sanctus est...</p>
+<a href="?category=1&amp;page=3">mehr</a>
+<br />-----------------------<br />
+
+<a href="?category=2&amp;page=1" title="Example Page 1">
+Example Page 1
+</a>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+invidunt ut labore et dolore magna aliquyam erat, ur sadipscing elitr,
+Stet clita kasd...</p>
+<a href="?category=2&amp;page=1">mehr</a>
+<br />-----------------------<br />
+
+?>
